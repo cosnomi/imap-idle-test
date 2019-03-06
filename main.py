7f75f2905cd537b0364b0769801eb08b0aae319b
Cosnomi <cosnomi@cosnomi.com>
2019-03-06 22:55:02 +0900

    Fetch latest emails based on last_check_datetime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from imapclient import IMAPClient
 import yaml
+from datetime import datetime, timedelta
 
 with open('config.yml') as f:
     data = yaml.load(f)
@@ -7,7 +8,7 @@ with open('config.yml') as f:
     USERNAME = data['USERNAME']
     PASSWORD = data['PASSWORD']
 
-server = IMAPClient(HOST)
+server = IMAPClient(HOST, use_uid=True)
 server.login(USERNAME, PASSWORD)
 server.select_folder('INBOX')
 
@@ -18,9 +19,27 @@ while True:
     server.idle()
     print("Connection is now in IDLE mode")
     try:
-        for i in range(connection_refresh_sec//timeout):
-                responses = server.idle_check(timeout=timeout)
-                print("Server sent:", responses if responses else "nothing")
+        for i in range(connection_refresh_sec // timeout):
+            last_check_datetime = datetime.now() - timedelta(minutes=5)
+            # The delay of push notification from the server is larger than timeout (30sec), so you have to subtract several minutes.
+            responses = server.idle_check(timeout=timeout)
+            if responses:
+                server.idle_done()
+                messages = server.search(['SINCE', last_check_datetime])
+                print(messages)
+                fetched_messages_meta_dict = server.fetch(
+                    messages, ['INTERNALDATE'])
+                to_fetch_body_messages_list = []
+                for key, meta in fetched_messages_meta_dict.items():
+                    print(meta)
+                    if meta[b'INTERNALDATE'] >= last_check_datetime:
+                        print('added')
+                        to_fetch_body_messages_list.append(key)
+                res = server.fetch(to_fetch_body_messages_list,
+                                   ['BODY.PEEK[TEXT]'])
+                print(res)
+                server.idle()
+            print("Server sent:", responses if responses else "nothing")
     except KeyboardInterrupt:
         break
     server.idle_done()
